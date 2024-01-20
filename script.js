efd52d1159fc6bef7f4ccf6d98da5634d84b77f0
P3DR0H3NR1QU3 <pedrohenriquesousa1523@gmail.com>
2024-01-20 18:26:21 -0300

    projeto FILMES

---
+++ script.js
@@ -0,0 +1,35 @@
+function buscar(){
+    const apiKey = '790af7bc';
+    const tituloFilme = document.getElementById('tituloFilme').value;
+
+    if(!tituloFilme){
+        alert('Insira o título do filme');
+        return;
+    }
+    
+    const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${tituloFilme}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na solicitação. Código de status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.Response === 'True') {
+                document.getElementById('error').src =data.Poster;
+                document.getElementById('titulo').textContent =data.Title;
+                document.getElementById('sinopse').textContent =data.Plot;
+                document.getElementById('genero').textContent =data.Genre;
+                document.getElementById('duracao').textContent =data.Runtime;
+                document.getElementById('lancamento').textContent =data.Year;
+            } else {
+                document.getElementById('error').src = 'imagem_triste-removebg-preview.png'
+                document.getElementById('titulo').textContent = 'Filme não encontrado!'
+            }
+        })
+        .catch(error => console.error (`Erro na solicitação: ${error.message}`));
+
+
+}
